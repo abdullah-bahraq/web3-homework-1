5e54c93f3c8b72c26c3bda0948e8e45c1593c754
Abdullah A. Bahraq <abdallahbhrag@gmail.com>
2023-05-10 17:01:29 +0300

    Final Project

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using _1.Models;
+
+namespace _1.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult ReverseName(string name)
+    {
+        if (name == null)
+        {
+            ViewBag.Message = "Please enter a valid name.";
+        }
+        else
+        {
+            string reversedName = "";
+            for (int i = name.Length - 1; i >= 0; i--)
+            {
+                reversedName += name[i];
+            }
+
+            ViewBag.Message = reversedName;
+        }
+        
+        return View("Index");
+    }
+}
+
